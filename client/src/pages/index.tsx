f40bed334d5be83de8b3876f4230ee1cfa41dd2a
Shinetopia <ShinetopiaOfficial@outlook.com>
2025-07-02 20:38:54 +0800

    Issue 19 Implement Figma colour palettes and custom font styles (#20)
    * Added Figma text styles for css & tailwind use
    
    * Added Figma colour palette
    
    * changed style jsx to inline variable declaration
    
    * hotfix: added missing font variable
    
    * quick hotfix: worksans variable not set properly
    
    ---------
    
    Co-authored-by: Jayshira <jason.yulfan@gmail.com>

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -24,7 +24,9 @@ export default function Home() {
         fontSans.variable,
       )}
     >
-      <h1 className="text-3xl text-primary">Test title</h1>
+      {/* Delete after style testing */}
+      <p className="subtitle text-primary">subtitle with primary colour</p>
+      <h1 className="title-large text-dark">Test Title</h1>
       <Button onClick={() => setClicked(true)}>
         {isLoading ? "Loading" : "Ping"}
       </Button>
