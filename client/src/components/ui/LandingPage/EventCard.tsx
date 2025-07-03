7638be97b7cccfb772b303ebc25d248dad8e41e9
mas-ruk <doruk.maslen@gmail.com>
2025-07-03 03:41:26 +0000

    feat: applied styling conventions drawn from main and added further detail into each event card

--- client/src/components/ui/LandingPage/EventCard.tsx
+++ client/src/components/ui/LandingPage/EventCard.tsx
@@ -2,19 +2,19 @@ import Image from "next/image";
 
 export default function EventCard() {
   return (
-    <div className="outline-solid flex items-center rounded-3xl p-4 outline outline-1 outline-gray-950">
+    <div className="flex items-center gap-4 rounded-3xl p-6 outline outline-1 outline-gray-950">
       {/* Text */}
-      <div className="flex-auto">
-        <p className="text-gray-500"> Monday - 9 July 2025</p>
-        <h2 className="text-2xl font-bold"> Group Stages Start </h2>
-        <div className="flex items-center gap-2">
+      <div className="min-w-0 flex-[2] pr-4">
+        <p className="body-lg mb-1 text-secondary">Monday - 9 July 2025</p>
+        <h2 className="subtitle mb-3">Group Stages Start</h2>
+        <div className="mb-3 flex items-center gap-2">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
             strokeWidth={1.5}
             stroke="currentColor"
-            className="size-6"
+            className="size-5 flex-shrink-0 text-secondary"
           >
             <path
               strokeLinecap="round"
@@ -27,16 +27,38 @@ export default function EventCard() {
               d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z"
             />
           </svg>
-          <p className="text-gray-500">Melbourne Convention Centre</p>
+          <p className="text-text">Melbourne Convention Centre</p>
         </div>
+        <div className="mb-3 flex items-center gap-2">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="size-5 flex-shrink-0 text-secondary"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+            />
+          </svg>
+          <p className="text-text">5:00pm - 8:00pm</p>
+        </div>
+        <p className="line-clamp-3 text-text">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac ante
+          rhoncus, hendrerit dui eget, pulvinar purus. Donec rutrum euismod
+          consequat.
+        </p>
       </div>
       {/* Image */}
-      <div className="relative h-[200px] w-[200px] flex-auto">
+      <div className="relative h-[200px] min-w-[180px] flex-[1]">
         <Image
           src="/placeholder-svg.svg"
           alt="placeholder-svg"
           fill
-          className="rounded-2xl object-contain"
+          className="rounded-2xl object-cover"
           priority
         />
       </div>
--- client/src/components/ui/LandingPage/index.tsx
+++ client/src/components/ui/LandingPage/index.tsx
@@ -5,11 +5,11 @@ import EventCard from "./EventCard";
 export default function LandingPage() {
   return (
     <div>
-      <h1 className="text-dark text-center text-5xl font-bold">
+      <h1 className="title-large text-center font-bold text-dark">
         {" "}
         Drone Competition Schedule{" "}
       </h1>
-      <div className="relative mx-auto h-[80px] w-[800px]">
+      <div className="relative mx-auto h-[80px] w-[1100px]">
         <Image
           src="/ProgressBar1.svg"
           alt="Progress Bar"
@@ -18,8 +18,8 @@ export default function LandingPage() {
           priority
         />
       </div>
-      <p className="pt-8">Our next competition is: </p>
-      <p className="pb-8 text-2xl font-bold">Monday - July 9th, 2025</p>
+      <p className="medium-lg pt-8">Our next competition is: </p>
+      <p className="subtitle pb-8 font-bold">Monday - July 9th, 2025</p>
 
       <EventCard />
     </div>
