c203a76fcc0a285d596d9a4005ec2f366dad551a
anqi <1004370869@qq.com>
2025-07-09 16:01:59 +0800

    Create models.py

--- server/api/models.py
+++ server/api/models.py
@@ -4,35 +4,41 @@ from django.db import models
 
 
 class Match(models.Model):
+    match_id = models.AutoField(primary_key=True)
     match_name = models.CharField(max_length=255)
     match_date = models.DateTimeField()
-    created_at = models.DateTimeField(auto_now_add=True)
+    group_id = models.IntegerField()
+    stage_id = models.InteferField()
+    
 
     def __str__(self):
         return self.match_name
 
-
 class MatchResult(models.Model):
-    match = models.ForeignKey("Match", on_delete=models.CASCADE)
-    team = models.ForeignKey("Team", on_delete=models.CASCADE)
+    match_result_id = models.AutoField(primary_key=True)
+    match = models.ForeignKey(Match, on_delete=models.CASCADE)
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+    
+    white_pins = models.IntegerField(null=True, blank=True)
+    penalty_pins = models.IntegerField(null=True, blank=True)
+    yellow_cards = models.IntegerField(null=True, blank=True)
+    red_cards = models.IntegerField(null=True, blank=True)
+    
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    p1_position = models.IntegerField(null=True, blank=True)
+    p2_position = models.IntegerField(null=True, blank=True)
+    honor_point = models.IntegerField(null=True, blank=True)
+    point = models.IntegerField(null=True, blank=True)
+    completed_time_second = models.IntegerField(null=True, blank=True)
 
-    honor_points = models.IntegerField()
-    white_pins = models.IntegerField()
-    penalty_pins = models.IntegerField()
-    yellow_cards = models.IntegerField()
-    red_cards = models.IntegerField()
-    mission_time_seconds = models.IntegerField()
-    regular_points = models.IntegerField()
-    final_rank = models.IntegerField()
-
+    def __str__(self):
+        return f"{self.team.team_name} in {self.match.match_name}"
+    
+class Team(models.Model):
+    team_id = models.AutoField(primary_key=True)
+    team_name = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.match} - {self.team}"
-
-
-class Team(models.Model):
-    name = models.CharField(max_length=255)
-
-    def __str__(self):
-        return self.name
+        return self.team_name  
