ede537af0ab47feba37c2d02e7d11e2ee21413d7
Joel Brooker <joeltbrooker4@gmail.com>
2025-07-12 05:55:00 +0000

    feat(api): add basic serializer for Match model

---
+++ server/api/match/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from .models import Match
+
+
+class MatchSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Match
+        fields = "__all__"
+        read_only_fields = ("match_id")
