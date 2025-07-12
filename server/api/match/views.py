20ba2cac49700add3ed82ec8ba162a8b9ca3ce47
Joel Brooker <joeltbrooker4@gmail.com>
2025-07-12 04:43:34 +0000

    feat(API): moved API functionality from leaderboard to match application

--- server/api/match/views.py
+++ server/api/match/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render  # noqa
+from rest_framework.decorators import api_view
+
+from django.http import JsonResponse
+
+
+leaderboard_data = {
+    "honor_point": 1,
+    "regular_point": 10,
+    "completed_time_second": 100
+    # other details needed for FrontEnd
+}
+
 
 # Create your views here.
+@api_view(["GET"])
+def leaderboard(request):
+    return JsonResponse(leaderboard_data, status=200)
