2436187fa0d8590e8fc25fdbc8ccd9d03e66d42d
JoJohowl <526767849@qq.com>
2025-07-18 23:10:42 +0800

    Clean up URL configuration formatting
    Removed unnecessary whitespace and fixed minor formatting issues in sponsor and main API URL configuration files for improved readability.

--- server/api/sponsor/urls.py
+++ server/api/sponsor/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from api.sponsor.views import SponsorListView, GuestListView 
+from api.sponsor.views import SponsorListView, GuestListView
 
-  
+
 urlpatterns = [
     path('', SponsorListView.as_view(), name='sponsor-list'),
     path('guests/', GuestListView.as_view(), name='guest-list'),
