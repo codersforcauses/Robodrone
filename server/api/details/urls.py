05359c004c99f650f86bf622976486a9d316af20
Jayshira <jason.yulfan@gmail.com>
2025-07-19 08:03:25 +0000

    Match Detail API added: - api/details/stages: get a list of stages for the dropdown - api/details/groups: get a list of groups for the dropdown
    - api/details/group_stage/<int:groupid>: get table objects filtered by group
    - api/details/knockout_stage/<int:stageid>: get knockout stage objects filtered by stageid (cant take 0 as a stageid since it is used for group stage)
    
    How the data is structured for group/knockout stage:
    [
        [
            {team1 row details}, (one team row)
            {team2 row details},
        ], (one table)
        [
            {team2 row details},
            {team3 row details},
        ]
    ]

---
+++ server/api/details/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import MatchResultsByGroup, MatchResultsByStage, get_groups, get_stages
+
+urlpatterns = [
+    path('group_stage/<int:groupid>/', MatchResultsByGroup.as_view(), name='match-results-by-group'),
+    path('knockout_stage/<int:stageid>/', MatchResultsByStage.as_view(), name='match-results-by-stage'),
+    path('groups/', get_groups, name='get-groups'),
+    path('stages/', get_stages, name='get-stages')
+]
