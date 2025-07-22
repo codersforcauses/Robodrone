3283ce3b8dda9272cb1658fb6de8ea729f1b7777
samueltl21 <samueltheodore21@gmail.com>
2025-07-22 10:48:58 +0000

    add save to model

--- server/api/match/models.py
+++ server/api/match/models.py
@@ -36,9 +36,26 @@ class MatchResult(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     p1_position = models.IntegerField(null=True, blank=True)
     p2_position = models.IntegerField(null=True, blank=True)
-    honor_point = models.IntegerField(null=True, blank=True)
-    point = models.IntegerField(null=True, blank=True)
+    honor_point = models.IntegerField(null=True, blank=True, editable=False)
+    point = models.IntegerField(null=True, blank=True, editable=False)
     completed_time_second = models.IntegerField(null=True, blank=True)
 
     def __str__(self):
         return f"{self.team} in {self.match}"
+
+    def save(self, *args, **kwargs):
+        # Default to 0 if any field is None
+        wp = self.white_pins or 0
+        pp = self.penalty_pins or 0
+        yc = self.yellow_cards or 0
+        rc = self.red_cards or 0
+
+        self.point = wp * 2 + pp * -3 + yc * -3 + rc * -10
+        self.honor_point = self._get_honor_point(
+            self.p1_position, self.p2_position)
+        super().save(*args, **kwargs)
+
+    def _get_honor_point(self, p1_position, p2_position):
+        if not p1_position or p1_position != 1:
+            return 0
+        return 1 + (4 - (p2_position or 0))
